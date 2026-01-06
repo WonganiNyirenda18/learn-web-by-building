6e5aa3414418ec6a218f23275272ef69b6511559
wongani18 <wonganinyirenda18@gmail.com>
2026-01-06 10:44:52 +0200

    feat: scaffold initial React application with App and Header components and basic styling.

---
+++ src/Components/Header.jsx
@@ -0,0 +1,16 @@
+export default function Header() {
+    return (
+        <header>
+            <div>
+                <nav>
+                    <ul>
+                        <li>Introduction</li>
+                        <li>Architecture</li>
+                        <li>Accessibility</li>
+                        <li>API's</li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    )
+}
