52b6766a89885c5ed3c607a1aab4e5e5150eddf3
wongani18 <wonganinyirenda18@gmail.com>
2026-01-21 10:24:09 +0200

    feat: modified project components

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
 import React from "react";
 import Header from "./Components/Header";
-import Hero from "./Components/Hero";
-import WebElements from "./Components/WebElements";
-import Technologies from "./Components/Technologies";
-import JackOfAllTrades from "./Components/JackOfAllTrades";
-import Footer from "./Components/Footer";
 
 function App() {
   return (
     <div className="min-h-screen bg-white">
       <Header />
-      <Hero />
-      <WebElements />
-      <Technologies />
-      <JackOfAllTrades />
-      <Footer />
+
     </div>
   );
 }
