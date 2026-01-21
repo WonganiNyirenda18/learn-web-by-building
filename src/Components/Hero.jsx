cc956b8393f7ead5efa95ed2032c1664caeea820
wongani18 <wonganinyirenda18@gmail.com>
2026-01-21 10:59:25 +0200

    feat: add Hero component displaying "The Nature of Web" title and introductory text.

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,6 +1,16 @@
 export default function Hero() {
     return (
         <section>
+            <div>
+                <div>
+                    <h1>The Nature of Web</h1>
+                    <p>
+                        Only through a fundamental understanding of the core technology
+                        can developers be expected to grow with the technological changes
+                        associated with web development
+                    </p>
+                </div>
+            </div>
 
         </section>
     );
