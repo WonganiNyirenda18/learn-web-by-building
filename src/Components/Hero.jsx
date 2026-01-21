b42ee4b50bf2f00dc46aad86b13efabf6fac4834
wongani18 <wonganinyirenda18@gmail.com>
2026-01-21 11:08:11 +0200

    feat: add new Hero component with title, description, and read more button

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,7 +1,7 @@
 export default function Hero() {
     return (
-        <section>
-            <div>
+        <section className="bg-[#EBF7F8] min-h-[600px] flex items-center pt-20">
+            <div className="max-w-7xl mx-auto px-6 grid md:grid-cols-2 items-center w-full">
                 <div>
                     <h1>The Nature of Web</h1>
                     <p>
@@ -9,6 +9,7 @@ export default function Hero() {
                         can developers be expected to grow with the technological changes
                         associated with web development
                     </p>
+                    <button>Read More</button>
                 </div>
             </div>
 
