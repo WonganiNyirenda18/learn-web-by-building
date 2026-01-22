2dfc6bc16cd35295b7d42d0b32256a65a936df82
wongani18 <wonganinyirenda18@gmail.com>
2026-01-22 11:17:13 +0200

    feat: structured data for easy scaling

--- src/Components/WebElements.jsx
+++ src/Components/WebElements.jsx
@@ -1,4 +1,23 @@
 export default function WebElements() {
+
+    const elements = [
+        {
+            number: "1",
+            title: "Markup",
+            desc: "formats and cross-references hypertext document"
+        },
+        {
+            number: "2",
+            title: "Protocol",
+            desc: "allows for the retrieval of linked resources from across the web"
+        },
+        {
+            number: "3",
+            title: "Notation",
+            desc: "URI uniquely identifies a resource and tells how to access it"
+        }
+    ];
+
     return (
         <section>
             <h1>Elements of the Web</h1>
