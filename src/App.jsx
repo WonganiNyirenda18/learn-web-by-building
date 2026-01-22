ddf4b2a37cb621a60fd496e9c9afbfb2e609f85b
wongani18 <wonganinyirenda18@gmail.com>
2026-01-22 11:04:48 +0200

    feat: added boiler component of web elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Header from "./Components/Header";
 import Hero from "./Components/Hero";
+import WebElements from "./Components/WebElements";
 
 function App() {
   return (
     <div className="min-h-screen bg-white">
+
       <Header />
       <Hero />
+      <WebElements />
 
     </div>
   );
---
+++ src/Components/WebElements.jsx
@@ -0,0 +1,7 @@
+export default function WebElements() {
+    return (
+        <section>
+            <h1>Elements of the Web</h1>
+        </section>
+    );
+}
