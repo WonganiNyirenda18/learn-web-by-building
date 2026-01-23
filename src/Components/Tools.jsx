e890d13766e90b99be22e83c3383306009df6800
wongani18 <wonganinyirenda18@gmail.com>
2026-01-23 23:31:45 +0200

    feat: creating a data array so we can map its elements straight to UI

--- src/Components/Tools.jsx
+++ src/Components/Tools.jsx
@@ -2,6 +2,37 @@ import reactCardImage from "../Assets/react.jpg";
 import cssCardImage from "../Assets/css.jpg"
 
 export default function Tools() {
+
+    const cards = [
+        {
+            id: 1,
+            type: "text",
+            title: "Technologies used",
+            items: [
+                "HTML, CSS, JavaScript",
+                "Libraries i.e React",
+                "Frameworks i.e Next.js",
+            ],
+            bg: "#EBF7F8",
+        },
+        {
+            id: 2,
+            type: "image",
+            image: reactCardImage,
+            alt: "React",
+            buttonText: "Learn React",
+            bg: "#EBF7F8",
+        },
+        {
+            id: 3,
+            type: "image",
+            image: cssCardImage,
+            alt: "CSS",
+            buttonText: "Learn CSS",
+            bg: "#EBF7F8",
+        },
+    ];
+
     return (
         <section className="py-12 bg-white">
             <div className="max-w-7xl mx-auto px-6 grid md:grid-cols-3 gap-6">
