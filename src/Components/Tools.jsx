ec960b386d865b7dd570aedb52efb3785a6b87aa
wongani18 <wonganinyirenda18@gmail.com>
2026-01-23 09:28:18 +0200

    feat: styled the first card

--- src/Components/Tools.jsx
+++ src/Components/Tools.jsx
@@ -1,11 +1,11 @@
 export default function Tools() {
     return (
-        <section>
-            <div>
+        <section className="py-12 bg-white">
+            <div className="max-w-7xl mx-auto px-6 grid md:grid-cols-3 gap-6">
 
-                <div>
-                    <h2>Technologies used</h2>
-                    <ul>
+                <div className="bg-[#EBF7F8] p-10 rounded-2xl flex flex-col justify-center">
+                    <h2 className="text-gray-900 text-2xl font-bold mb-6">Technologies used</h2>
+                    <ul className="text-gray-500 text-sm space-y-3">
                         <li>HTML, CSS, JavaScript</li>
                         <li>Libraries i.e React</li>
                         <li>Frameworks i.e Next.js</li>
