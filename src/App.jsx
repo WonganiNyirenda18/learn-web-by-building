01003c2e9b67a860e50c8d476f25a1ef44710e4d
wongani18 <wonganinyirenda18@gmail.com>
2026-01-23 09:13:47 +0200

    feat: Added the tools component displaying the technoligie used for web

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Components/Header";
 import Hero from "./Components/Hero";
 import WebElements from "./Components/WebElements";
+import Tools from "./Components/Tools";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Hero />
       <WebElements />
+      <Tools />
 
     </div>
   );
---
+++ src/Components/Tools.jsx
@@ -0,0 +1,9 @@
+export default function Tools() {
+    return (
+        <section>
+            <div>
+                <h2>Technologies used</h2>
+            </div>
+        </section>
+    );
+}
