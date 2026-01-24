37d58ff376a1bdbdee7ca760c93f2e25a5c5e3c8
wongani18 <wonganinyirenda18@gmail.com>
2026-01-24 21:09:16 +0200

    feat: Create new Concepts component to display related web development concepts.

--- src/Components/Concepts.jsx
+++ src/Components/Concepts.jsx
@@ -2,7 +2,27 @@ export default function Concepts() {
     return (
         <section>
             <div>
+
                 <h2>Relatad Concepts</h2>
+
+                <div>
+                    <h3> Be a jack of all trades and a master of all</h3>
+                    <p>
+                        Only through a fundamental understanding of the core technology
+                        can developers be expected to grow with technological
+                        changes associated with web development
+                    </p>
+                </div>
+
+                <div>
+                    <h3>Related concepts</h3>
+                    <ul>
+                        <li>Information Architecture</li>
+                        <li>Digital Accessibility</li>
+                        <li>API's</li>
+                    </ul>
+                </div>
+
             </div>
         </section>
     );
