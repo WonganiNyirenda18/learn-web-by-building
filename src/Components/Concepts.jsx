68864beb5999c66769cdb6fea187fc8e9455425d
wongani18 <wonganinyirenda18@gmail.com>
2026-01-25 21:28:54 +0200

    feat: applying style to the other section of the component

--- src/Components/Concepts.jsx
+++ src/Components/Concepts.jsx
@@ -15,25 +15,26 @@ export default function Concepts() {
                     </div>
                 </div>
 
-                <h2>Relatad Concepts</h2>
+                <div className="space-y-6">
+                    <div>
+                        <h3> Be a jack of all trades and a master of all</h3>
+                        <p>
+                            Only through a fundamental understanding of the core technology
+                            can developers be expected to grow with technological
+                            changes associated with web development
+                        </p>
+                    </div>
 
-                <div>
-                    <h3> Be a jack of all trades and a master of all</h3>
-                    <p>
-                        Only through a fundamental understanding of the core technology
-                        can developers be expected to grow with technological
-                        changes associated with web development
-                    </p>
+                    <div>
+                        <h3>Related concepts</h3>
+                        <ul>
+                            <li>Information Architecture</li>
+                            <li>Digital Accessibility</li>
+                            <li>API's</li>
+                        </ul>
+                    </div>
                 </div>
 
-                <div>
-                    <h3>Related concepts</h3>
-                    <ul>
-                        <li>Information Architecture</li>
-                        <li>Digital Accessibility</li>
-                        <li>API's</li>
-                    </ul>
-                </div>
 
             </div>
         </section>
